4baf5b99dbe113a9e4d134e1d88d587de0263637
Ali Ahsan <aliahsan8751@gmail.com>
2026-03-14 09:19:44 +0500

    connected database with web api

---
+++ DTOs/CreateShortUrlRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UrlShortener.Api.DTOs
+{
+    public class CreateShortUrlRequest
+    {
+        [Required]
+        public string Url { get; set; } = string.Empty;
+    }
+}
